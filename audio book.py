0fd79d44a39bcd4109c56d85bebcd57cf569e211
Sneara Parvin <130774997+Sneara0@users.noreply.github.com>
2023-07-27 07:58:45 -0700

    Add files via upload

---
+++ audio book.py
@@ -0,0 +1,6 @@
+import pyttsx3
+import PyPDF2
+
+friend=pyttsx3.init()
+friend.say(' Method overloading refers to having multiple methods with the same name that accept different sets of argument Given a single method or function ')
+friend.runAndWait()
